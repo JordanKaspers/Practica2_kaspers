0000d8efd9dd0320b41719390e5d8764a5516a52
Jordan Kaspers <kaspers@esat-alumni.com>
2018-05-28 15:21:16 +0200

    Guard now able to change states: patrolling, chasing and suspicious. Added projectile for the player to spawn when throwing a projectile

---
+++ Practica2_kaspers/Source/Practica2_kaspers/Private/Projectile.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Projectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+AProjectile::AProjectile()
+{
+  // Using sphere collision
+  CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+  CollisionComp->InitSphereRadius(5.0f);
+  CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
+  CollisionComp->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);		// set up a notification for when this component hits something blocking
+
+  // Players can't walk on it
+  CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
+  CollisionComp->CanCharacterStepUpOn = ECB_No;
+
+  // Set as root component
+  RootComponent = CollisionComp;
+
+  // Using a ProjectileMovementComponent for movement
+  ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
+  ProjectileMovement->UpdatedComponent = CollisionComp;
+  ProjectileMovement->InitialSpeed = 4000.f;
+  ProjectileMovement->MaxSpeed = 4000;
+  ProjectileMovement->bRotationFollowsVelocity = false;
+  ProjectileMovement->bShouldBounce = true;
+
+  // Die after 3 seconds by default
+  InitialLifeSpan = 3.0f;
+}
+
+void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+  // Only add impulse and destroy projectile if we hit a physics
+  if ((OtherActor != NULL) && (OtherActor != this) && (OtherComp != NULL) && OtherComp->IsSimulatingPhysics())
+  {
+    OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
+    Destroy();
+  }
+}
